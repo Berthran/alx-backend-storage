0dec7ff21d15080084fff036658b568ad39e6885
Daniel Berthran <berthlearn@gmail.com>
2024-10-23 18:13:34 +0100

    task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''
+A redis class
+'''
+
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    '''
+    A storage class with Redis
+    '''
+
+    def __init__(self) -> None:
+        '''
+        Constructor method
+        '''
+        self._redis: redis.Redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''
+        takes a data and returns a string
+        '''
+        randKey: str = str(uuid.uuid4())
+        self._redis.set(randKey, data)
+        return (randKey)
